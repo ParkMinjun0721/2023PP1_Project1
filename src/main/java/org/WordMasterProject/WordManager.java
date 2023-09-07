6dd9e4a190ef101f8e925918496118b0f3c0ae69
MinjunPark <22000257@handong.ac.kr>
2023-09-07 17:34:45 +0900

    Word Master Project

--- src/main/java/org/WordMasterProject/WordManager.java
+++ src/main/java/org/WordMasterProject/WordManager.java
@@ -13,28 +13,30 @@ public class WordManager {
             int menu = selectMenu();
 
             if(menu == 0){
+                System.out.println("Exit the Program.\n" +
+                        "See you again :)");
                 break; //나가기
             }
             else if(menu == 1) { //모든 단어 보기
                 wordCRUD.listAll();
             }
             else if(menu == 2) { //수준별 단어 보기
-
+                ment();
             }
             else if(menu == 3) { //단어 검색
-
+                ment();
             }
             else if(menu == 4) { //단어 추가
                 wordCRUD.addItem();
             }
             else if(menu == 5) { //단어 수정
-
+                ment();
             }
             else if(menu == 6) { //단어 삭제
-
+                ment();
             }
             else if(menu == 7) { //파일 저장
-
+                ment();
             }
         }
     }
@@ -52,6 +54,9 @@ public class WordManager {
                 "********************\n" +
                 "=> 원하는 메뉴는? ");
         return  s.nextInt();
+    }
+    public void ment(){
+        System.out.println("Not implemented yet.");
     }
 
 }
